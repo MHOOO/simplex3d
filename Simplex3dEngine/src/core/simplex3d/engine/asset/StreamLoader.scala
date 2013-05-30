62cc2ce3dbeca12704ca204e799d302f5c1d5343
Thomas Karolski <Thomas.Karolski@googlemail.com>
2013-05-30 09:02:39 +0200

    - throw error when an unsupported face has been parsed - fix bug when normals is empty but not texcoords

--- Simplex3dEngine/src/core/simplex3d/engine/asset/StreamLoader.scala
+++ Simplex3dEngine/src/core/simplex3d/engine/asset/StreamLoader.scala
@@ -32,16 +32,16 @@ import simplex3d.data.double._
 
 
 trait StreamLoader extends AssetLoader {
-  
+
   protected def openStream(path: String) :InputStream
-  
+
   protected def load[T](path: String)(loadStream: InputStream => T) :Option[T] = {
     var stream: InputStream = null
     try {
       stream = openStream(path)
       if (stream == null) {
         println(path + " not found")//XXX log
-        None 
+        None
       }
       else  Some(loadStream(stream))
     }
@@ -57,48 +57,48 @@ trait StreamLoader extends AssetLoader {
       if (stream != null) stream.close()
     }
   }
-  
+
   def loadRgbImg(path: String) :Option[(ConstVec2i, RgbTextureData)] = {
     load(path) { stream =>
       val img = ImageIO.read(stream)
-      
+
       val width = img.getWidth
       val height = img.getHeight
-      
+
       val data = DataBuffer[Vec3, UByte](width*height)
       val buffer = data.buffer()
-      
+
       var y = 0; while (y < height) {
         var x = 0; while (x < width) {
-          
+
           val pixel = img.getRGB(x, y)
           val index = (x + (height - 1 - y)*width)*3
           buffer.put(index, (pixel >> 16).toByte)
           buffer.put(index + 1, (pixel >> 8).toByte)
           buffer.put(index + 2, pixel.toByte)
-          
+
           x += 1
         }
         y += 1
       }
-      
+
       (ConstVec2i(width, height), data)
     }
   }
-  
+
   def loadRgbaImg(path: String) :Option[(ConstVec2i, RgbaTextureData)] = {
     load(path) { stream =>
       val img = ImageIO.read(stream)
-      
+
       val width = img.getWidth
       val height = img.getHeight
-      
+
       val data = DataBuffer[Vec4, UByte](width*height)
       val buffer = data.buffer()
-      
+
       var y = 0; while (y < height) {
         var x = 0; while (x < width) {
-          
+
           val i = img.getRGB(x, y)
           val pixel = img.getRGB(x, y)
           val index = (x + (height - 1 - y)*width)*4
@@ -106,16 +106,16 @@ trait StreamLoader extends AssetLoader {
           buffer.put(index + 1, (pixel >> 8).toByte)
           buffer.put(index + 2, pixel.toByte)
           buffer.put(index + 3, (pixel >> 24).toByte)
-          
+
           x += 1
         }
         y += 1
       }
-      
+
       (ConstVec2i(width, height), data)
     }
   }
-  
+
   def loadObj(path: String)
   :Option[(
     DataBuffer[SInt, Unsigned],// indices
@@ -125,19 +125,24 @@ trait StreamLoader extends AssetLoader {
   )] = {
     load(path) { stream =>
       val reader = new BufferedReader(new InputStreamReader(stream))
-      
+
       val verticesBuilder = ArrayBuilder.make[Vec3]()
       val normalsBuilder = ArrayBuilder.make[Vec3]()
       val texCoordsBuilder = ArrayBuilder.make[Vec2]()
-      
+
       val indexBuilders = Array(
         ArrayBuilder.make[Array[Int]](),
         ArrayBuilder.make[Array[Int]](),
         ArrayBuilder.make[Array[Int]]()
       )
-      
-      def decodeIndex(group: String) :Array[Int] = group.split("/").map(_.toInt - 1)
-      
+
+      def decodeIndex(group: String) :Array[Int] = group.split("/").map(num => {
+          num match {
+              case "" => 1
+              case num => num.toInt
+          }
+      } - 1)
+
       var lineNumber = 1
       var line = reader.readLine(); while (line != null) {
         val tokens: Seq[String] = line.split("\\s")
@@ -150,20 +155,23 @@ trait StreamLoader extends AssetLoader {
           case Seq("v", x, y, z) => verticesBuilder += Vec3(x.toDouble, y.toDouble, z.toDouble)
           case Seq("vn", x, y, z) => normalsBuilder += Vec3(x.toDouble, y.toDouble, z.toDouble)
           case Seq("vt", x, y) => texCoordsBuilder += Vec2(x.toDouble, y.toDouble)
-          case Seq("f", _*) => throw new RuntimeException(
+          //case Seq("f", _*) => { ... }// TODO: this is bugged in 2.9.2, works in 2.10, change after updating.
+          //case _ => // ignore
+          case _ => {
+            if (tokens.size > 0 && tokens(0) == "f") throw new RuntimeException(
                 "Unsupported polygon format in obj model '" + path +
                 "' on line " + lineNumber + ", only triangles are supported.")
-          case _ => // ignore
+          }
         }
 
         lineNumber += 1
         line = reader.readLine()
       }
-      
+
       val vertices = verticesBuilder.result()
       val normals = normalsBuilder.result()
       val texCoords = texCoordsBuilder.result()
-      
+
       val data = {
         val builder = ArrayBuilder.make[IndexedSeq[Object]]()
         builder += vertices
@@ -171,25 +179,25 @@ trait StreamLoader extends AssetLoader {
         if (!normals.isEmpty) builder += normals
         builder.result()
       }
-      
+
       val index = indexBuilders.map(_.result())
       val vertexMap = new HashMap[IndexedSeq[Object], Integer]
       val resultIndexBuilder = ArrayBuilder.make[Int]()
-      
+
       var count = 0
       var n = 0; while (n < index(0).length) {
         var k = 0; while (k < 3) {
           val group = index(k)(n)
-          
+
           val vertexDataArray = new Array[Object](3)
           var i = 0; while (i < data.length) {
-            
+
             val idx = if (i < group.length) group(i) else group(0)
             vertexDataArray(i) = data(i)(idx)
-            
+
             i += 1
           }
-          
+
           val vertexData: IndexedSeq[Object] = vertexDataArray
           val existing = vertexMap.get(vertexData)
           val vertexId = if (existing != null) existing.toInt else {
@@ -198,15 +206,15 @@ trait StreamLoader extends AssetLoader {
             vertexMap.put(vertexData, id)
             id
           }
-          
+
           resultIndexBuilder += vertexId
           k += 1
         }
-        
+
         n += 1
       }
-      
-      
+
+
       def reverseMap[K, V](map: HashMap[K, V]) :HashMap[V, K] = {
         val result = new HashMap[V, K]
         val iter = map.entrySet.iterator
@@ -217,35 +225,35 @@ trait StreamLoader extends AssetLoader {
         result
       }
       val lookup = reverseMap(vertexMap)
-      
-      
+
+
       val resultIndexArray = resultIndexBuilder.result();
       val resultIndices = IndexBuffer(count, resultIndexArray.length)
       resultIndices.put(resultIndexArray)
 
       val resultVertices = DataBuffer[Vec3, RFloat](count)
       val resultNormals = if (normals.isEmpty) None else Some(DataBuffer[Vec3, RFloat](count))
-      val resultTexCoords = if (normals.isEmpty) None else Some(DataBuffer[Vec2, RFloat](count))
-      
+      val resultTexCoords = if (texCoords.isEmpty) None else Some(DataBuffer[Vec2, RFloat](count))
+
       var i = 0; while (i < count) {
         val data = lookup.get(i)
-        
+
         resultVertices(i) = data(0).asInstanceOf[Vec3]
-        
+
         var next = 1
-        
+
         if (resultTexCoords.isDefined) {
           resultTexCoords.get(i) = data(next).asInstanceOf[Vec2]
           next += 1
         }
-        
+
         if (resultNormals.isDefined) {
           resultNormals.get(i) = data(next).asInstanceOf[Vec3]
         }
-        
+
         i += 1
       }
-      
+
       (resultIndices, resultVertices, resultNormals, resultTexCoords)
     }
   }
